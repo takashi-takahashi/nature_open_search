c92b6d89118fd9e52b957c2acb8a3b8d8b9308df
Takashi Takahashi <takahashi.t.cc@m.titech.ac.jp>
2018-01-01 17:08:19 +0900

    update sample module

--- executor.py
+++ executor.py
@@ -3,9 +3,12 @@ import search_api.open_search_api as search_api
 
 
 def main():
+    # for each index one can feed (RELATION, "TERM") tuple
+    # search result is given with json format
     search_result = search_api.search(cql_keywords=("=", "physics quantum"), dc_title=("=", "physics"),
                                       maximum_records=100)
 
+    # a sample code to show titles
     entry_list = search_result["feed"]["entry"]
     for entry in entry_list:
         print(entry["title"])
