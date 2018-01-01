9d2f9f22e1ca53a814567a95fed0a644013a523a
Takashi Takahashi <takahashi.t.cc@m.titech.ac.jp>
2018-01-01 15:17:59 +0900

    initial commit

---
+++ executor.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+import search_api.open_search_api as open_search
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ search_api/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+from . import open_search_api
+
+__all__ = ['open_search_api']
