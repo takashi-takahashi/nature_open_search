1ddcb3eb019e32983f7521e068f3f43333dcd6e7
Takashi Takahashi <takahashi.t.cc@m.titech.ac.jp>
2018-01-01 17:04:50 +0900

    add sample execution file

--- executor.py
+++ executor.py
@@ -1,9 +1,14 @@
 # coding=utf-8
-import search_api.open_search_api as open_search
+import search_api.open_search_api as search_api
 
 
 def main():
-    pass
+    search_result = search_api.search(cql_keywords=("=", "physics quantum"), dc_title=("=", "physics"),
+                                      maximum_records=100)
+
+    entry_list = search_result["feed"]["entry"]
+    for entry in entry_list:
+        print(entry["title"])
 
 
 if __name__ == "__main__":
